533cae513cbf1e819f278fd29d7846e8c237134a
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2018-07-16 20:05:13 +0200

    Improved

--- audiowave.sh
+++ audiowave.sh
@@ -3,7 +3,15 @@ TITLE=""
 SUBTITLE=""
 WIDTH="640"
 HEIGHT="480"
-COLOR="yellow"
+MODE="line" # point, line, p2p, cline
+WAVE_WIDTH="640"
+WAVE_HEIGHT="480"
+WAVE_COLOR="yellow"
+WAVE_X=-1
+WAVE_Y=-1
+TEXT_COLOR="yellow"
+TEXT_X=-1
+TEXT_Y=-1
 AUDIO=""
 
 POSITIONAL=()
@@ -42,8 +50,48 @@ case $key in
     shift # past argument
     shift # past value
     ;;
-    -c|--color)
-    COLOR="$2"
+    -ww|--wave_width)
+    WAVE_WIDTH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -wh|--wave_height)
+    WAVE_HEIGHT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -wc|--wave_color)
+    WAVE_COLOR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -wx|--wave_x)
+    WAVE_X="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -wy|--wave_y)
+    WAVE_Y="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -tc|--text_color)
+    TEXT_COLOR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -tx|--text_x)
+    TEXT_X="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -ty|--text_y)
+    TEXT_Y="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -m|--mode)
+    MODE="$2"
     shift # past argument
     shift # past value
     ;;
@@ -60,16 +108,44 @@ done
 
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
+if [ $WAVE_X -eq -1 ]
+then
+    WAVE_X="$((($WIDTH-$WAVE_WIDTH)/2))"
+fi
+if [ $WAVE_Y -eq -1 ]
+then
+    WAVE_Y="$((($HEIGHT-$WAVE_HEIGHT)/2))"
+fi
+echo $TEXT_X $TEXT_Y
+if [ $TEXT_X -eq -1 ]
+then
+    TEXT_X=20
+fi
+if [ $TEXT_Y -eq -1 ]
+then
+    TEXT_Y="$(($HEIGHT-100))"
+fi
+echo $TEXT_X $TEXT_Y
+TEXT_Y2="$(($TEXT_Y + 50))"
 if [ ${#AUDIO} -eq 0 ] || [ ! -f $AUDIO ] 
 then
+    echo $AUDIO
     echo "Usage ./audiowave.sh [-t title] [-s subtitle] [-b background_image] -a audio [-w width] [-h height] [-c color]"
     echo "-t set the title of the video. It's optional"
     echo "-s set the subtitle of the video. It's optional"
     echo "-b set the background_image. It's optional"
     echo "-a set the audio. It's mandatory"
+    echo "-m set the mode. Default is line. Possible modes: point, line, p2p, cline"
     echo "-w set the width of the video. It's optional, default width is 640 px"
     echo "-h set the height of the video. It's optional, default height is 480 px"
-    echo "-c set the color of title and subtitle. It's optional, default color is yellow"
+    echo "-ww set the width of the wave. It's optional, default width is 640 px"
+    echo "-wh set the height of the wave It's optional, default height is 480 px"
+    echo "-wc set the color of wave. It's optional, default color is yellow"
+    echo "-wx set the x position of wave. It's optional, default x position is in the middle"
+    echo "-wy set the y position of wave. It's optional, default y position is in the middle"
+    echo "-tc set the color of title and subtitle. It's optional, default color is yellow"
+    echo "-tx set the x position of text. It's optional, default x position is 20 px"
+    echo "-ty set the y position of text. It's optional, default y position is 100 px from bottom"
     exit 126
 fi
 
@@ -78,23 +154,21 @@ then
     rm output.mp4
 fi
 
+
 if [ ${#BACKGROUND} -ne 0 ] && [ -f $BACKGROUND ]
 then
-    title_position=${HEIGHT}-80
-    subtitle_position=${HEIGHT}-30
-    convert $BACKGROUND -resize $WIDTHx$HEIGHT temporal.jpg
-    convert temporal.jpg -font Ubuntu -weight 700 -pointsize 48 -draw "fill ${COLOR} text 10, ${title_position} '${TITLE}'" temporal.jpg
-    convert temporal.jpg -font Ubuntu -weight 700 -pointsize 48 -draw "fill ${COLOR} text 10, ${subtitle_position} '${SUBTITLE}'" temporal.jpg
-    ffmpeg -loop 1 -i temporal.jpg -i ${AUDIO} -c:v libx264 -tune stillimage -c:a libmp3lame -q:a 4 -pix_fmt yuv420p -shortest temporal_video.mp4
-    ffmpeg -i ${AUDIO} -filter_complex "[0:a]showwaves=s=${WIDTH}x${HEIGHT}:mode=cline,format=yuv420p[v]" -map "[v]" -map 0:a -c:v libx264 -an temporal_onda.mp4
-    ffmpeg -i temporal_video.mp4 -i temporal_onda.mp4 -filter_complex " \
-        [0:v]setpts=PTS-STARTPTS, scale=${WIDTH}x${HEIGHT}[top]; \
-        [1:v]setpts=PTS-STARTPTS, scale=${WIDTH}x${HEIGHT}, \
-        format=yuva420p,colorchannelmixer=aa=0.5[bottom]; \
-        [top][bottom]overlay=shortest=1" -c:a libmp3lame -c:v libx264 output.mp4
-    rm temporal.jpg
-    rm temporal_video.mp4
-    rm temporal_onda.mp4
+    ffmpeg -y -i ${AUDIO} -loop 1 -i $BACKGROUND -filter_complex "\
+        [0:a]showwaves=size=${WAVE_WIDTH}x${WAVE_HEIGHT}:mode=${MODE}:colors=${WAVE_COLOR}:rate=30,format=rgba[top];\
+        [1:v]setpts=PTS-STARTPTS, scale=${WIDTH}x${HEIGHT}, format=rgba[bottom];\
+        [bottom][top]overlay=format=auto:y=${WAVE_Y}:x=${WAVE_X}[temporal];\
+        [temporal]drawtext=text='${TITLE}':fontcolor=${TEXT_COLOR}:fontsize=30:font=Ubuntu:x=${TEXT_X}:y=${TEXT_Y}[l1];\
+        [l1]drawtext=text='${SUBTITLE}':fontcolor=${TEXT_COLOR}:fontsize=30:font=Ubuntu:x=${TEXT_X}:y=${TEXT_Y2}[output]\
+        " -map "[output]" -pix_fmt yuv420p -map 0:a -c:v libx264 -c:a libmp3lame -shortest output.mp4
 else
-    ffmpeg -i $PWD/${AUDIO} -filter_complex "[0:a]showwaves=s=${WIDTH}x${HEIGHT}:mode=cline,format=yuv420p[v]" -map "[v]" -map 0:a -c:v libx264 -c:a libmp3lame -q:a 4 output.mp4
+    ffmpeg -y -i ${AUDIO} -filter_complex "\
+        [0:a]showwaves=size=${WAVE_WIDTH}x${WAVE_HEIGHT}:mode=${MODE}:colors=${WAVE_COLOR}:rate=30,format=rgba[top];\
+        [top]drawtext=text='${TITLE}':fontcolor=${TEXT_COLOR}:fontsize=30:font=Ubuntu:x=${TEXT_X}:y=${TEXT_Y}[l1];\
+        [l1]drawtext=text='${SUBTITLE}':fontcolor=${TEXT_COLOR}:fontsize=30:font=Ubuntu:x=${TEXT_X}:y=${TEXT_Y2}[output]\
+        " -map "[output]" -pix_fmt yuv420p -map 0:a -c:v libx264 -c:a libmp3lame -shortest output.mp4
 fi
+
